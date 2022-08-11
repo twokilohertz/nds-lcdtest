c79cfe0a126cb224f7d2c7026cde7aca4d31cafa
Adam Macdonald <twokilohertz@cock.li>
2022-08-11 21:52:00 +0100

    setting up bg0

--- src/init.c
+++ src/init.c
@@ -10,14 +10,14 @@ bool init_video(void)
 {
     powerOn(POWER_LCD | POWER_2D_A);
 
-    vramSetPrimaryBanks(VRAM_A_MAIN_BG_0x06000000,
+    vramSetBankA(VRAM_A_MAIN_BG_0x06000000);
+
+    /* vramSetPrimaryBanks(VRAM_A_MAIN_BG_0x06000000,
                         VRAM_B_MAIN_BG_0x06020000,
                         VRAM_C_SUB_BG_0x06200000,
-                        VRAM_D_LCD);
+                        VRAM_D_LCD); */
 
-    videoSetMode(MODE_5_2D | DISPLAY_BG2_ACTIVE);
-
-    videoSetMode(MODE_5_2D | DISPLAY_BG3_ACTIVE);
+    videoSetMode(MODE_5_2D | DISPLAY_BG0_ACTIVE);
     
     return true;
 }
--- src/main.c
+++ src/main.c
@@ -5,11 +5,18 @@
 
 int main(void)
 {
+    // Initialise parts of NDS we need
     init();
+
+    // Initialise background for displaying test patterns
+    int bg_id = bgInit(0, BgType_Bmp16, BgSize_B16_256x256, 0, 0);
+
+    // todo: copy to vram, display on screen
 
     while (true)
     {
-        printf("hello\n");
+        swiWaitForVBlank();
+        
     }
 
     return 0;
