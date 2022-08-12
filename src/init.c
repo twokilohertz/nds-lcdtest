c113aed6160a33c9e4a03e951b55ca4a0c6ef0c1
Adam Macdonald <twokilohertz@cock.li>
2022-08-12 14:50:43 +0100

    cleanup init functions, remove deprecated/commented code

--- src/init.c
+++ src/init.c
@@ -12,11 +12,6 @@ bool init_video(void)
 
     vramSetBankA(VRAM_A_MAIN_BG_0x06000000);
 
-    /* vramSetPrimaryBanks(VRAM_A_MAIN_BG_0x06000000,
-                        VRAM_B_MAIN_BG_0x06020000,
-                        VRAM_C_SUB_BG_0x06200000,
-                        VRAM_D_LCD); */
-
     videoSetMode(MODE_5_2D | DISPLAY_BG0_ACTIVE);
     
     return true;
