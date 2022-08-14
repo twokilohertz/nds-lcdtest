1e00cdd6243ae213970ad8136fa5aa453d57f334
Adam Macdonald <twokilohertz@cock.li>
2022-08-15 00:12:53 +0100

    added some more comments

--- src/init.c
+++ src/init.c
@@ -8,11 +8,14 @@ bool init(void)
 
 bool init_video(void)
 {
+    // Power on the LCDs and 2D graphic core(s)
     powerOn(POWER_LCD | POWER_ALL_2D);
 
-    vramSetBankA(VRAM_A_MAIN_BG_0x06000000);
-    vramSetBankC(VRAM_C_SUB_BG_0x06200000);
+    // Map VRAM banks
+    vramSetBankA(VRAM_A_MAIN_BG_0x06040000); // Bank A, main engine, background 2
+    vramSetBankC(VRAM_C_SUB_BG_0x06200000); // Bank C, sub engine, background 0
 
+    // Set video modes
     videoSetMode(MODE_5_2D | DISPLAY_BG2_ACTIVE);
     videoSetModeSub(MODE_0_2D | DISPLAY_BG0_ACTIVE);
     
