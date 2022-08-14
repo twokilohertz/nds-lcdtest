4bb0186af574b6ad76ef972164605dea0c7e0c41
Adam Macdonald <twokilohertz@cock.li>
2022-08-15 00:00:38 +0100

    improvements & setup for game

--- src/init.c
+++ src/init.c
@@ -8,11 +8,13 @@ bool init(void)
 
 bool init_video(void)
 {
-    powerOn(POWER_LCD | POWER_2D_A);
+    powerOn(POWER_LCD | POWER_ALL_2D);
 
     vramSetBankA(VRAM_A_MAIN_BG_0x06000000);
+    vramSetBankC(VRAM_C_SUB_BG_0x06200000);
 
-    videoSetMode(MODE_5_2D | DISPLAY_BG0_ACTIVE);
+    videoSetMode(MODE_5_2D | DISPLAY_BG2_ACTIVE);
+    videoSetModeSub(MODE_0_2D | DISPLAY_BG0_ACTIVE);
     
     return true;
 }
--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include "platform.h"
 #include "init.h"
+#include "res/smpte_colour_bars.h"
 
 #include <stdio.h>
 
@@ -8,15 +9,23 @@ int main(void)
     // Initialise parts of NDS we need
     init();
 
-    // Initialise background for displaying test patterns
-    int bg_id = bgInit(0, BgType_Bmp16, BgSize_B16_256x256, 0, 0);
+    // Initialise console for printing text
+    PrintConsole console;
+    consoleInit(&console, 0, BgType_Text4bpp, BgSize_T_256x256, 0, 0, false, true);
 
-    // todo: copy to vram, display on screen
+    // Initialise background for displaying test patterns
+    int bg2_id = bgInit(2, BgType_Bmp16, BgSize_B16_256x256, 0, 0);
+
+    dmaCopy((const void*)smpte_colour_barsBitmap, bgGetGfxPtr(bg2_id), 256*256);
 
     while (true)
     {
-        swiWaitForVBlank();
-        
+        swiWaitForVBlank(); // Frame has been displayed, we can draw again :)
+
+        iprintf("Hello, World!\n");
+
+        scanKeys();
+        if (keysDown() & KEY_START) break;
     }
 
     return 0;
